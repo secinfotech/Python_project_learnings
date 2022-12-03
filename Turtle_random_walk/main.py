665b776bd0f9e9b0cfd057fc76e8f0d2cd54c44e
secinfotech <aritra1309@gmail.com>
2022-12-03 08:16:15 +0530

    latest

--- Turtle_random_walk/main.py
+++ Turtle_random_walk/main.py
@@ -1,6 +1,21 @@
 import turtle as t
 import random
 tim = t.Turtle()
-colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray",
-          "SeaGreen"]
+t.colormode(255)
+directions = [0, 90, 180, 270]
+tim.width(20)
+tim.speed("fastest")
 
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    changing_color = (r, g, b)
+    return changing_color
+
+
+for _ in range(500):
+    tim.color(random_color())
+    tim.forward(50)
+    tim.setheading(random.choice(directions))
